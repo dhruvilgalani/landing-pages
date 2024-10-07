afc95c8255548d33f0746c82ed617983651f711d
dhruvilgalani <dhruvilgalani1@gmail.com>
2024-10-07 21:41:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        primary : "SingleDay",
+        display : "swap",
+      
+      }
+    },
+  },
+  plugins:[], 
+}
+
